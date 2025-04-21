196ebbac61861780ca8c388c4bb8c21dc6fc48c6
Audalio Devops <audalio.devops@gmail.com>
2025-04-21 15:55:47 -0300

    form Formulario componentes sem acao

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,15 +3,17 @@ import React from "react";
 interface ButtonProps {
     style? : string;
     label? : string;
+    type? : "submit" | "button" | "reset" | undefined;
     onClick?: (event: any) => void;
 }
 
 export const Button: React.FC <ButtonProps> = ({
-    style, label, onClick    
+    style, label, onClick, type    
 } : ButtonProps) => {
     return (
         <button className={`${style} text-white px-4 py-2 rounded-lg`} 
-                            onClick={onClick}>
+                type={type}
+                onClick={onClick}>
             { label }
         </button>        
     )
